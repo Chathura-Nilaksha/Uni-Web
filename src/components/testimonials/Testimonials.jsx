5aed3d89ecea3c85cc651e5a11a7c4ae13838bb8
Chathura-Nilaksha <cnilakshami@gmail.com>
2024-12-14 20:43:47 +0530

    Completed Testimonials file

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,19 +1,105 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Testimonials.css'
-// import next_icon from '../../assets/next_icon.png'
-// import back_icon from '../../assets/back_icon.png'
-// import user_1 from '../../assets/user_1.png'
-// import user_2 from '../../assets/user_1.png'
-// import user_3 from '../../assets/user_1.png'
-// import user_4 from '../../assets/user_1.png'
+import next_icon from '../../assets/next_icon.png'
+import back_icon from '../../assets/back_icon.png'
+import user_1 from '../../assets/user_1.jpg'
+import user_2 from '../../assets/user_2.jpg'
+import user_3 from '../../assets/user_3.jpg'
+import user_4 from '../../assets/user_4.jpg'
 
 const Testimonials = () => {
+  const slider = useRef();
+  let tx = 0;
+
+  const slideForward = () => {
+    if(tx>-50){tx -= 25;}
+    slider.current.style.transform = `translateX(${tx}%)`;
+  }
+
+  const slideBackward = () => {
+    if(tx<0){tx += 25;}
+    slider.current.style.transform = `translateX(${tx}%)`;
+  }
+
   return (
     <div className='testimonials'>
-      <img src={next_icon} alt="" className='next-btn'/>
-      <img src={back_icon} alt="" className='back-btn'/>
+      <img src={next_icon} alt="" className='next-btn'
+        onClick={slideForward}/>
+      <img src={back_icon} alt="" className='back-btn'
+        onClick={slideBackward}/>
       <div className="slider">
-        
+        <ul ref={slider}>
+
+          <li>
+
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="" />
+                <div>
+                  <h3>Sumudu Sathira</h3>
+                  <span>Former student of Our University</span>
+                </div>
+              </div>
+              <p>
+                Here insert the things he is saying about the University.
+              </p>
+            </div>
+
+          </li>
+
+          <li>
+
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_2} alt="" />
+                <div>
+                    <h3>Ranil Sathira</h3>
+                    <span>Former student of Our University</span>
+                </div>
+              </div>
+              <p>
+                Here insert the things he is saying about the University.
+              </p>
+            </div>
+
+          </li>
+
+          <li>
+
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_3} alt="" />
+                <div>
+                    <h3>Kasun Sathira</h3>
+                    <span>Former student of Our University</span>
+                </div>
+              </div>
+              <p>
+                Here insert the things he is saying about the University.
+              </p>
+            </div>
+
+          </li>
+
+          <li>
+
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="" />
+                  <div>
+                    <h3>Chanaka Sathira</h3>
+                    <span>Former student of Our University</span>
+                  </div>
+              </div>
+              <p>
+                Here insert the things he is saying about the University.
+              </p>
+            </div>
+
+          </li>
+
+
+        </ul>
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -11,4 +11,26 @@ createRoot(document.getElementById('root')).render(
 
 // 12.8---23.21-Headers.jsx
 // 12.9---stoped 1.09. but stop checking-again at 1.03.38
-// 12.11--stoped 1.23.49 testi
+// 12.11--stoped 1.32.42 testi
+
+/////////////////////////
+// NOTES: CSS
+
+// when "," -> oonama ekakata adalai.
+//      " " (without comma) -> mehi thiyana hema ekakma thiyena 
+//                              HTML tag valata adalava. 
+//Ex-1
+// .slider ul {CSS} --> /*uses where both slider cls and ul tag nested way
+//                        slider yatathe ul. */
+//Ex-2
+// .slider ul li {CSS} --> mehi thiyana hema ekakma thiyena HTML tag valata adalava.
+//Ex-3
+// .next-btn, .back-btn {CSS}  -->  /*Appling to any of theses clses */
+
+
+
+//  HOVER ACTION          .program:hover .caption {
+//                            opacity: 1;
+//                            padding-top: 0;
+//                        }  
+                    
