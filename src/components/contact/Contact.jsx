188cc00d87ce7f9c5a3cb9c36e96aeb02e5505ac
Chathura-Nilaksha <cnilakshami@gmail.com>
2024-12-15 19:31:20 +0530

    Completed the contact us page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,20 +2,61 @@ import React from 'react'
 import './Contact.css'
 
 const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "86486beb-2d2e-420f-b638-37cc42b7cac5");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            console.log("Success", data);
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+
   return (
     <div className='contact'>
         <div className="contact-col">
             <h3>Send us a message</h3>
+
+            {/* Im not inserting img for "send us a message" */}
             <p>Your feedback, questions and Suggestions 
                 are important to us.</p>
             <ul>
+                {/* Im not inserting imges for each list */}
                 <li>contact@gmail.com</li>
                 <li>O77-123456789</li>
                 <li>No.20, Galle Road, Colombo 3.</li>
             </ul>
         </div>
 
-        <div className="contact-col"></div>
+        <div className="contact-col">
+            <form onSubmit={onSubmit}>
+                <label>Your Name</label>
+                <input type='text' name='name' placeholder='Enter Your Name' required/>
+                <label>Phone number</label>
+                <input type='tel' name='phone' placeholder='Enter Your Contact Number' required/>
+                <label>Write Your Message here</label>
+                <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+                <button type='submit' className='btn dard-btn'>Submit now</button>
+            </form>
+            <span>{result}</span>
+        </div>
       
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,23 @@ createRoot(document.getElementById('root')).render(
 // 12.8---23.21-Headers.jsx
 // 12.9---stoped 1.09. but stop checking-again at 1.03.38
 // 12.11--stoped 1.32.42 testi
+// 12.14--completed above page
+//      --stated contact us jsx -1.18.53
+    // NET- ???? 
+  //   const slideForward = () => {
+  //   if(tx>-50){tx -= 25;}
+  //   slider.current.style.transform = `translateX(${tx}%)`; <--??? THIS LINE-NET
+  // }
+
+// 12.15--sto---1.55.53-  2.03.36
+
+
+/////////////////////////
+// SPECIAL FEATURES, LIBRARIES, APIs USED IN THIS PROJECT
+
+// 1.web3forms --> For contact us forms
+//      This helps to send inquiry messages (form submitions) to my 
+//      registering email address.
 
 /////////////////////////
 // NOTES: CSS
