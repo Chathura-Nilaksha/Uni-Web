91cb839f96286bdc77ff163017be61b780869674
Chathura-Nilaksha <cnilakshami@gmail.com>
2024-12-17 21:11:56 +0530

    Add @media queries to Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import NavPicture from "../../assets/react.svg";
 import { Link } from 'react-scroll';
+import menu_icon from '../../assets/menu_icon.png';
 
 const Navbar = () => {
 
@@ -13,12 +14,16 @@ const Navbar = () => {
     })
   },[]); 
   
+  const[mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false):setMobileMenu(true);
+  }
 
   return (
     <nav className={`container ${sticky ? "dark-nav":""}`}  > 
                     {/* N: above className is a special way.  */}
       <img className="logo" src={NavPicture} alt="" />
-      <ul>
+      <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-160} duration={500}>About us</Link></li>
@@ -27,6 +32,7 @@ const Navbar = () => {
         <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
               
       </ul>
+      <img onClick={toggleMenu} src={menu_icon} alt="Menu icon" className='menu_icon'/>
     </nav>
   )
 }
